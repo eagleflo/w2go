7b19869b94183a0f5c0e28478e8d612c3a9ada86
Aku Kotkavuo <aku@hibana.net>
2011-01-04 13:29:58 +0200

    Reverted earlier Sinatra-related fixes.

--- w2go.rb
+++ w2go.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/env ruby
 # w2go.rb, the main Sinatra app for w2go.
 
-require 'rubygems'
 require 'sinatra'
 require 'haml'
 
 set :haml, {:format => :html5}
-set :public, File.dirname(__FILE__) + "/public"
-set :views, File.dirname(__FILE__) + "/views"
 set :run, true
 
 get '/' do
